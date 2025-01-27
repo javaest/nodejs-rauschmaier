1cb6d05ab4085e3f422526677e1419e7194a2ad1
BU9000 <126714607+BU9000@users.noreply.github.com>
2025-01-27 18:03:07 +0000

    änderungen

--- src/index.js
+++ src/index.js
@@ -3,6 +3,31 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+
+function isAuthorized(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || authHeader !== 'secretpassword') {
+    return res.status(401).send('Unauthorized: Access Denied');
+  }
+
+  next();
+}
+
+
+app.get('/', isAuthorized, (req, res) => res.send('Hello World!'));
+
+app.get('/users', (req, res) => {
+  res.json([
+    {
+      id: 1,
+      name: 'User Userson'
+    },
+  ]);
+});
+
+
 let users = [
   { id: 1, name: 'Alice' },
   { id: 2, name: 'Bob' }
@@ -15,6 +40,22 @@ app.get('/', (req, res) => {
 // Beispielroute
 app.get('/api/users', (req, res) => {
   res.json(users);
+});
+
+
+app.post('/api/users', isAuthorized, (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res.status(400).send('Bad Request: Name is required');
+  }
+
+  const newUser = {
+    id: users.length + 1,
+    name
+  };
+
+  users.push(newUser);
+  res.status(201).json(newUser);
 });
 
 // Server starten
